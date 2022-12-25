b983fd0212db7131b9d9764d659b25d0e17be317
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2022-12-25 14:57:11 +0200

    Added JWT options validation

--- src/BusinessLogic/Options/JwtOptions.cs
+++ src/BusinessLogic/Options/JwtOptions.cs
@@ -1,7 +1,15 @@
-﻿namespace BusinessLogic.Options;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessLogic.Options;
 
 public sealed class JwtOptions
 {
+    private const int MinimumHmac256KeyLength = 16;
+
+    [Required, StringLength(100, MinimumLength = MinimumHmac256KeyLength, 
+        ErrorMessage = "Minimum length of the key is 16 characters")]
     public string Key { get; init; }
+
+    [Required, Range(1, 1000, ErrorMessage = "Lifetime minutes should be between 1 and 1000 inclusive")]
     public int LifetimeMinutes { get; init; }
 }
