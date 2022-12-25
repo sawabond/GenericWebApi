9f0ffb22be17316fe9a58cea1682a30779cc367a
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2022-12-25 19:50:19 +0200

    Added validators for login and register. Added business layer validation

---
+++ src/api/BusinessLogic/Validation/AppUser/LoginValidator.cs
@@ -0,0 +1,14 @@
+﻿using BusinessLogic.Models.AppUser;
+using FluentValidation;
+using static BusinessLogic.Validation.AppUser.Rules;
+
+namespace BusinessLogic.Validation.AppUser;
+
+internal sealed class LoginValidator : AbstractValidator<LoginModel>
+{
+	public LoginValidator()
+	{
+		RuleFor(x => x.UserName).NotEmpty().Length(3, 20);
+		RuleFor(x => x.Password).NotEmpty().Length(6, 50).Matches(PasswordRegex);
+	}
+}
---
+++ src/api/BusinessLogic/Validation/AppUser/Rules.cs
@@ -0,0 +1,6 @@
+﻿namespace BusinessLogic.Validation.AppUser;
+
+internal static class Rules
+{
+    public const string PasswordRegex = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$";
+}
